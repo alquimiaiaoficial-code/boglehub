6cba4aa080ca921e834c2a12e21427bd554bf412
alquimiaiaoficial <alquimiaia.oficial@gmail.com>
2026-05-13 20:25:29 +0200

    chore: test config + deployment files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,11 @@ import path from 'path'
 
 export default defineConfig({
   test: {
-    environment: 'node',
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
+    globals: true,
   },
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    alias: { '@': path.resolve(__dirname, './src') },
   },
 })
